5c94d22febdc333c35f7bef72b83c1c9495418e4
padrisimo <nopuedoparardemolar@outlook.com>
2017-03-31 17:57:13 +0200

    get the feed from stocks apis

--- www/js/services.js
+++ www/js/services.js
@@ -2,13 +2,32 @@ angular.module('finApp.services', [])
 
 .factory('stockDataService', function($q, $http) {
 
+  var getDetailsData  = function (ticker) {
+    var deferred = $q.defer(),
+      url="http://query.yahooapis.com/v1/public/yql?format=json&env=store://datatables.org/alltableswithkeys&q=select * from yahoo.finance.quote where symbol in ('"+ ticker +"')";
+
+    $http.get(url)
+      .success(function(json){
+        var jsonData = json.query.results.quote;
+        deferred.resolve(jsonData);
+
+      })
+      .error(function(error){
+        console.log("details data error: " + error);
+        deferred.reject();
+
+      });
+
+    return deferred.promise;
+  };
+
 	var getPriceData = function(ticker){
 		var deferred = $q.defer(),
-      url="http://query.yahooapis.com/v1/public/yql?format=json&env=store://datatables.org/alltableswithkeys&q=select * from yahoo.finance.quote where symbol in ('"+ ticker +"')";
+      url="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22"+ticker+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
 
 		$http.get(url)
 	      .success(function(json){
-	      	var jsonData = json.query.results.quote;
+	      	var jsonData = json.query.results.quote.PriceSales;
 	        deferred.resolve(jsonData);
 
 	      })
@@ -23,7 +42,8 @@ angular.module('finApp.services', [])
 
 
 	return {
-		getPriceData: getPriceData
+		getPriceData: getPriceData,
+    getDetailsData: getDetailsData
 	};
 
 })
